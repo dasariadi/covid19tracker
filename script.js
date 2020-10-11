59b696acc86a243a5ee00b07aa0b87816f88034b
Dasa Riadi <DASA.RIADI97@GMAIL.COM>
2020-10-11 17:00:34 -0400

    final commit with current date displayed

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // defining variables
 
+var today = new Date();
+console.log(today);
+document.getElementById('today').append(today)
+
 var myForm = document.getElementById('myForm');
 // adding event listener
 myForm.addEventListener('submit', function(e){
 
     e.preventDefault()
-
     var country = document.getElementById('country').value;
     
     // input fetch request to get COVID-19 data
@@ -17,5 +20,16 @@ myForm.addEventListener('submit', function(e){
     .then((res) => {
         var length = res.length
         var index = length - 1
+        var confirmed = document.getElementById('confirmed')
+        var recovered = document.getElementById('recovered')
+        var deaths = document.getElementById('deaths')
+
+        confirmed.innerHTML = ''
+        recovered.innerHTML = ''
+        deaths.innerHTML = ''
+
+        confirmed.append("Total Confirmed: " + res[index].Confirmed)
+        recovered.append("Total Recovered: " + res[index].Recovered)
+        deaths.append("Total Deaths: " + res[index].Deaths)
     })
 });
